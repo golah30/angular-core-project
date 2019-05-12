7511748ce75f3a5afe8967b50b22d751f530fd3d
Dmitriy Kurilenko <golah30@gmail.com>
2019-05-12 18:27:25 +0300

    INT-98 Top pane,  mixins, logo and avatar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+
+interface User {
+    img: string;
+    initials: string;
+    name: string;
+}
 
 @Component({
     selector: "app-root",
     templateUrl: "./app.component.pug",
     styleUrls: ["./app.component.scss"]
 })
-export class AppComponent {
-    title = "acp-project";
+export class AppComponent implements OnInit {
+    title: string = "acp-project";
+    logoSrc: string = "../assets/images/logo.svg";
+    user: User = {
+        img: "../assets/images/avatar.png",
+        initials: "DK",
+        name: "Khristofer Khristorozhdestvenskii"
+    };
+    isSearch: boolean = false;
+    isMenu: boolean = false;
+
+    ngOnInit(): void {}
+    toggleSearch(): void {
+        this.isSearch = !this.isSearch;
+    }
+    toggleMenu(): void {
+        this.isMenu = !this.isMenu;
+    }
+    account(): void {}
 }

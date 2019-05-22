8c627e8819276534959f8039ed8b90b64bda1f6e
Dmitriy Kurilenko <golah30@gmail.com>
2019-05-22 20:14:33 +0300

    cleaning dashboard from shared components examples

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,68 +1,12 @@
-import { Component, OnInit, OnDestroy } from "@angular/core";
-interface Tag {
-    name: string;
-    isActive: boolean;
-}
-interface Comment {
-    id: string;
-    username: string;
-    text: string;
-}
+import { Component, OnInit } from "@angular/core";
+
 @Component({
     selector: "acp-dashboard",
     templateUrl: "./dashboard.component.pug",
     styleUrls: ["./dashboard.component.scss"]
 })
-export class DashboardComponent implements OnInit, OnDestroy {
+export class DashboardComponent implements OnInit {
     constructor() {}
-    date: Date = new Date();
-    comment: Comment = {
-        id: "12",
-        username: "Some user name",
-        text: "Lorem ipsum dolore"
-    };
-    tag: Tag = {
-        name: "some tag",
-        isActive: true
-    };
-    isScrolled: boolean = false;
-    isFavorite: boolean = false;
-    onLikeToggle(): void {
-        this.isFavorite = !this.isFavorite;
-    }
-    onTagSelect(tag: string): void {
-        this.tag.isActive = !this.tag.isActive;
-    }
 
-    scrollTop($element): void {
-        $element.scrollIntoView({
-            behavior: "smooth",
-            block: "start",
-            inline: "nearest"
-        });
-        this.isScrolled = false;
-    }
-    ngOnInit() {
-        window.addEventListener("scroll", this.scroll, true);
-    }
-    ngOnDestroy() {
-        window.removeEventListener("scroll", this.scroll, true);
-    }
-    submit(comment: Comment) {
-        console.log(comment);
-        if (comment.id) {
-            console.log("comment edited");
-        } else {
-            console.log("comment created");
-        }
-    }
-    scroll = (event: any): void => {
-        if (event.srcElement.classList.contains("main__content")) {
-            if (event.srcElement.scrollTop > 400 && !this.isScrolled) {
-                this.isScrolled = true;
-            } else if (event.srcElement.scrollTop < 400 && this.isScrolled) {
-                this.isScrolled = false;
-            }
-        }
-    };
+    ngOnInit() {}
 }

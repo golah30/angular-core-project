8ce68111d8cfe3cf5a4c52bc0893f060ac0f14f1
Dmitriy Kurilenko <golah30@gmail.com>
2019-06-07 18:04:30 +0300

    added onBlur search bar closing

--- src/app/core/top-pane/top-pane.component.ts
+++ src/app/core/top-pane/top-pane.component.ts
@@ -1,4 +1,13 @@
-import { Component, OnInit, Input, Output, EventEmitter } from "@angular/core";
+import {
+    Component,
+    OnInit,
+    Input,
+    Output,
+    EventEmitter,
+    ViewChild,
+    ElementRef,
+    Renderer2
+} from "@angular/core";
 import { User } from "../../interfaces";
 @Component({
     selector: "acp-top-pane",
@@ -6,22 +15,38 @@ import { User } from "../../interfaces";
     styleUrls: ["./top-pane.component.scss"]
 })
 export class TopPaneComponent implements OnInit {
-    constructor() {}
-
+    constructor(private _renderer: Renderer2) {}
+    @ViewChild("search") searchInput: ElementRef;
     @Input() user: User;
     @Input() isMenu: boolean;
     @Output() toggleMenu = new EventEmitter<boolean>();
 
     logoSrc: string = "../assets/images/logo.svg";
     isSearch: boolean = false;
-
+    closedByBlur: boolean = false;
     toggleSearch(): void {
-        this.isSearch = !this.isSearch;
+        if (!this.closedByBlur) {
+            this.searchInput.nativeElement.value = "";
+            this.isSearch = !this.isSearch;
+            if (this.isSearch) {
+                this.searchInput.nativeElement.focus();
+            }
+        }
     }
 
     toggleSideMenu(): void {
         this.toggleMenu.emit();
     }
     account(): void {}
-    ngOnInit() {}
+    ngOnInit() {
+        this._renderer.listen(this.searchInput.nativeElement, "blur", e => {
+            if (e.target.value === "") {
+                this.isSearch = false;
+                this.closedByBlur = true;
+            }
+            setTimeout(() => {
+                this.closedByBlur = false;
+            }, 200);
+        });
+    }
 }
